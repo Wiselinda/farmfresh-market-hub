6b579b91e3a47430f81f3569d9c57c14e48ad8c1
Wiselinda <zid2400@byui.edu>
2025-06-04 17:57:46 +0300

    fix change

--- config/swagger.js
+++ config/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000', // Change to Render URL after deployment
+        url: 'https://farmfresh-api.onrender.com', 
         description: 'Local server',
       },
     ],
